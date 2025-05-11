50a5ce2c39414bab25e8f3dff4deb21c3cde79b5
Vivek Kumar <vivekgupta70509@gmail.com>
2025-05-11 18:08:32 +0530

    Add files via upload

---
+++ Assignment32.py
@@ -0,0 +1,28 @@
+#Q1
+def sum(a,b):
+    return a+b
+print("sum is ",sum(3,5))
+#Q2
+def area(r):
+    return 3.14*r**2
+print("area is ",area(5))
+
+
+#Q3
+def avg(a,b,c):
+    return (a+b+c)/3
+print("avg is",avg(1,2,3))
+#Q4
+def compound(p,r,t):
+    return p*(1+r/100)**(t) - p
+print("compound interest is ",compound(1000,5,5))
+#Q5
+def volume(l,b,h):
+    return l*b*h
+print("Volume is",volume(2,3,4))
+
+
+
+
+
+
